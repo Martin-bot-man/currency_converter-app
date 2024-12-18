a8a3cb9702e87e9f42c1bf295e0312552e6bed94
Martin Odongo <martinwere14@gmail.com>
2024-12-18 18:16:20 +0300

    feat: created the currencydropdown component

--- src/components/CurrencyConverter.jsx
+++ src/components/CurrencyConverter.jsx
@@ -16,6 +16,7 @@ const CurrencyConverter = () => {
             const data= await res.json()
 
             setCurrencies(object.keys(data))
+            console.log(data);
 
         }catch(error){
             console.error('Error fetching', error);
@@ -28,23 +29,41 @@ const CurrencyConverter = () => {
     },[])
     console.log(currencies);
 
+    const ConvertCurrency=()=>{
+
+    };
+    const handleFavorite =(currency)=>{
+        //add to favoriters
+
+    }
+
     //currencies = 'https://api.frankfurter.app/latest?amount=1&from=USD&to=INR';
 
-  return (
-    <div className="max-w-xl mx-auto my-10 p-5 bg-white rounded-lg shadow-md">
+  return(
+     <div className="max-w-xl mx-auto my-10 p-5 bg-white rounded-lg shadow-md">
         <h2 className="mb-5 text-2xl font-semibold text-gray-700">Currency converter</h2>
         <div>
-            <CurrencyDropdown currencies={currencies} title='From:'/>
-            {/* swap currrecy button */}
-            <CurrencyDropdown currencies={currencies} title='To :'/>
+            <CurrencyDropdown 
+            currencies={currencies} 
+            currency ={fromCurrency}
+            setCurrency={setFromCurrency}
+            title='From:'
+            handleFavorite={handleFavorite}
+             />
+            {/* swap currecy button */}
+            <CurrencyDropdown 
+            currencies={currencies} 
+            setCurrency={settoCurrency}
+            title='To :'
+            handleFavorit= {handleFavorite}/>
         </div>
 
         <div mt-4>
             <label htmlFor="amount" className='block text-sm font-medium text-gray-700'>Amount</label>
-            <input type="number"  value={amount}className='w-full p-2 border-gray-300 rounded-md shadow-sm focus:outline-none  focus:ring-2 focus:ring-indigo-500 mt-1' onChange={(e)=>setAmount(e.target.value)}/>
+            <input type="number"  onChange={(e)=> setAmount(e.target.value)} value={amount} className='w-full p-2 border-gray-300 rounded-md shadow-sm focus:outline-none  focus:ring-2 focus:ring-indigo-500 mt-1' onChange={(e)=>setAmount(e.target.value)}/>
         </div>
         <div className="flex justify-end mt-6 ">
-            <button className ="px-5 py-2 bg-indigo-600 text-white rounded-md hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">Convert</button>
+            <button onClick={ConvertCurrency} className ="px-5 py-2 bg-indigo-600 text-white rounded-md hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">Convert</button>
         </div>
         <div className="mt-2 text-lg font-medium text-right text-green-600">Converted Amount: 69 USD</div>
     </div>
--- src/components/CurrencyDropdown.jsx
+++ src/components/CurrencyDropdown.jsx
@@ -1,18 +1,25 @@
 import React from 'react'
 import { FaRegStar } from "react-icons/fa";
 
-const CurrencyDropdown = (currencies, currency,setCurrency,favorites,handleFavorite,title="") => {
+const CurrencyDropdown = ({
+  currencies, 
+  currency,
+  setCurrency, 
+  favorites,
+  handleFavorite, 
+  title=""}) => {
   return (
     <div>
       <label htmlFor={title} className="block text-sm font-medium text-gray-700">{title}</label>
 
       <div className="mt-1 relative ">
-        <select className= "w-full p-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-indigo-500" >
+        <select value={currency} onChange={(e=>{setCurrency(e.target.value)})}className= "w-full p-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-indigo-500" >
           {/* render favorites */}
           <hr />
-          { currencies?.map((currency)=>{<option value={currency} key={currency}>{currency}</option>})}
+          { currencies?.map((currency)=>{
+            <option value={currency} key={currency}>{currency}</option>})}
         </select>
-        <button><FaRegStar /></button>
+        <button onClick= {()=>handleFavorite(currency)}className='absolute inset-y-0 pr-5 flex items-center text-sm leading-5'><FaRegStar /></button>
       </div>
     </div>
   )
